b9abd84590ff21eb89fed0e8b0b1882839aae545
Arcarnan <nataniel1993@hotmail.com>
2019-01-23 16:57:48 +0100

    added alternatives for loading scenes

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,12 +7,14 @@ public class MainMenu : MonoBehaviour
 {
 
 
-
+    // Loading the game screen
     public void LoadGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("GameStart");
     }
 
+    // Quitting the application
     public void QuitGame()
     {
         Debug.Log("GAME QUIT");
