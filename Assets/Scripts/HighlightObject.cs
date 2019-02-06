9ffb6eea2319be0166e7687feaa0d8f25dbc00d4
Arcarnan <nataniel1993@hotmail.com>
2019-02-06 10:19:57 +0100

    added a selection box for selected objects

--- Assets/Scripts/HighlightObject.cs
+++ Assets/Scripts/HighlightObject.cs
@@ -8,6 +8,7 @@ public class HighlightObject : MonoBehaviour, IPointerEnterHandler, IPointerExit
     public Color originalColor;
     private Image image;
 
+    public GameObject selectionBox;
     public Sprite sprite;
     public Sprite spriteHighlight;
     public Material mat;
@@ -18,6 +19,7 @@ public class HighlightObject : MonoBehaviour, IPointerEnterHandler, IPointerExit
     {
         image = GetComponent<Image>();
         originalColor = image.color;
+        //selectionBox = GetComponent<SpriteRenderer>().sprite;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -44,11 +46,13 @@ public class HighlightObject : MonoBehaviour, IPointerEnterHandler, IPointerExit
         {
             //image.sprite = spriteHighlight;
             image.material = mat;
+            selectionBox.SetActive(true);
         }
         else
         {
             //image.sprite = sprite;
             image.material = default;
+            selectionBox.SetActive(false);
         }
     }
 }
