19d13ae2b439d43a32437ba9030a287d4fa27f8e
Arcarnan <nataniel1993@hotmail.com>
2019-02-13 15:15:53 +0100

    connections between objects is (crudely) working now

--- Assets/Scripts/SelectedObject.cs
+++ Assets/Scripts/SelectedObject.cs
@@ -5,8 +5,8 @@ public class SelectedObject : MonoBehaviour
 {
     [Header("Connection line elements")]
     public GameObject connectionLine;
-    private RectTransform lineToEnd;
-    private RectTransform lineFromStart;
+    private Transform lineToEnd;
+    private Transform lineFromStart;
     private bool connectionStarted;
     private Vector3 connectionStartPos;
     private Vector3 connectionEndPos;
@@ -64,7 +64,6 @@ public class SelectedObject : MonoBehaviour
             {
                 ConnectObjects();
             }
-            //Debug.Log("Selected object is now " + newSelected.name);
         }
     }
 
@@ -73,72 +72,76 @@ public class SelectedObject : MonoBehaviour
         connectionStarted = true;
     }
 
+    /// <summary>
+    /// This function does the logic behind connecting two objects in the editor. 
+    /// It takes the previously selected gameobject and connects it with a visible line (2 image gameobjects) 
+    /// if a new gameobject is selected after the "connect" button is pressed.
+    /// </summary>
     public void ConnectObjects()
     {
         if (!connectionStarted)
         {
-            /// checks double to make sure function is not called from elsewhere
+            /// checks again to make sure function is not called from somewhere it should not
             return;
         }
         else
         {
-            GameObject connectionLineClone = Instantiate(connectionLine, canvas.transform);
-            connectionLineClone.transform.SetAsFirstSibling();
-            connectionLineClone.transform.position = oldSelected.transform.position;
-            lineToEnd = connectionLineClone.transform.Find("LineToEnd").GetComponent<RectTransform>();
-            lineFromStart = connectionLineClone.transform.Find("LineFromStart").GetComponent<RectTransform>();
-
-
             connectionStartPos = oldSelected.transform.position;
             connectionEndPos = selected.transform.position;
+
+            GameObject connectionLineClone = Instantiate(connectionLine, canvas.transform);
+            connectionLineClone.transform.SetAsFirstSibling();
+            connectionLineClone.transform.position = connectionStartPos;
+
+            lineToEnd = connectionLineClone.transform.Find("LineToEnd").GetComponent<RectTransform>().transform;
+            lineFromStart = connectionLineClone.transform.Find("LineFromStart").GetComponent<RectTransform>().transform;
+
             float XPosDiff = Mathf.Abs(connectionStartPos.x - connectionEndPos.x) / 100;        //100 pixels per unit
             float YPosDiff = Mathf.Abs(connectionStartPos.y - connectionEndPos.y) / 100;        //100 pixels per unit
-            Debug.Log("XPosDiff: " + XPosDiff + "YPosDiff: " + YPosDiff);
-
-            lineToEnd.transform.localScale = new Vector3(XPosDiff, 0.05f, 1.0f);
-            lineFromStart.transform.localScale = new Vector3(0.05f, YPosDiff, 1.0f);
-
-            Vector2 lineFromStartCenterPos = lineFromStart.rect.size;
-            Vector2 lineToEndCenterPos = lineToEnd.rect.size;
 
             /// connection to the right
             if (connectionStartPos.x <= connectionEndPos.x)
             {
+                lineToEnd.localScale = new Vector3(XPosDiff, 0.05f, 1.0f);
+                lineFromStart.localScale = new Vector3(0.05f, YPosDiff, 1.0f);
                 /// connection down
                 if (connectionStartPos.y >= connectionEndPos.y)
                 {
-                    lineToEnd.transform.position = new Vector3(lineFromStart.transform.position.x + XPosDiff * 50, connectionEndPos.y);
-                    lineFromStart.transform.position = new Vector3(connectionStartPos.x, selected.transform.position.y + YPosDiff * 50);
+                    lineFromStart.position = new Vector3(connectionStartPos.x, connectionEndPos.y + YPosDiff * 50);
+                    lineToEnd.position = new Vector3(lineFromStart.position.x + XPosDiff * 50, connectionEndPos.y);
                 }
                 ///connection up
                 else
                 {
                     connectionLineClone.transform.Rotate(new Vector3(0, 180, 180));
-                    //Debug.Log("Connection --up " + rotate);
+
+                    lineFromStart.position = new Vector3(connectionStartPos.x, connectionEndPos.y - YPosDiff * 50);
+                    lineToEnd.position = new Vector3(lineFromStart.position.x + XPosDiff * 50, connectionEndPos.y);
                 }
             }
             /// connection to the left
             else
             {
-                //Debug.Log("Connection --left");
+                lineFromStart.localScale = new Vector3(0.05f, XPosDiff, 1.0f);
+                lineToEnd.localScale = new Vector3(YPosDiff, 0.05f, 1.0f);
+
                 /// connection down
                 if (connectionStartPos.y >= connectionEndPos.y)
                 {
-                    connectionLineClone.transform.Rotate(new Vector3(0, 0, 270));
-                    //lineToEnd.transform.position = new Vector3(lineToEnd.transform.position.x, connectionEndPos.y);
-                    //Debug.Log("Connection down--");
+                    connectionLineClone.transform.Rotate(new Vector3(180, 180, 90));
+
+                    lineFromStart.position = new Vector3(connectionStartPos.x - XPosDiff * 50, connectionStartPos.y);
+                    lineToEnd.position = new Vector3(lineFromStart.position.x - XPosDiff * 50, connectionEndPos.y + YPosDiff * 50);
                 }
                 ///connection up
                 else
                 {
                     connectionLineClone.transform.Rotate(new Vector3(180, 0, 270));
-                    //Debug.Log("Connection --up    start: " + connectionStartPos.y + " end: " + connectionEndPos.y);
+
+                    lineFromStart.position = new Vector3(connectionStartPos.x - XPosDiff * 50, connectionStartPos.y);
+                    lineToEnd.position = new Vector3(lineFromStart.position.x - XPosDiff * 50, connectionEndPos.y - YPosDiff * 50);
                 }
             }
-
-
-
-
             ///reset after connection is esablished
             connectionStarted = false;
         }
